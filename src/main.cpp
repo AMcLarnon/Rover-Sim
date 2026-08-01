a7794aad09c7b8396dcfae67e438c2f55d472a6b
20118107 <20118107@setu.ie>
2026-08-01 22:12:24 +0100

    add README

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,36 @@ int main()
     int xPosition = 0;
     int yPosition = 0;
 
+    char command;
+
     std::cout << "Rover status:" << std::endl;
     std::cout << "Battery: " << battery << "%" << std::endl;
     std::cout << "Position: (" << xPosition << ", " << yPosition << ")" << std::endl;
+
+    std::cout << "Enter movement command (w/a/s/d): ";
+    std::cin >> command;
+
+    if (command == 'w')
+    {
+        yPosition++;
+    }
+
+    if (command == 's')
+    {
+        yPosition--;
+    }
+
+    if (command == 'a')
+    {
+        xPosition--;
+    }
+
+    if (command == 'd')
+    {
+        xPosition++;
+    }
+
+    std::cout << "New position: (" << xPosition << ", " << yPosition << ")" << std::endl;
 
     return 0;
 }
